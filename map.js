f7bafbadf4aba49ed1efaacf57ac651e127d4484
mynameislolao <lolaot3ro@gmail.com>
2025-03-20 00:43:35 +0000

    files

---
+++ map.js
@@ -0,0 +1,329 @@
+function showModalone(){
+    $(".modal1").toggleClass("show")
+
+}
+
+$(".highbarnet").click(showModalone)
+
+function showModaltwo(){
+    $(".modal2").toggleClass("show")
+}
+
+$(".tottridge").click(showModaltwo)
+
+function showModalthree(){
+    $(".modal3").toggleClass("show")
+}
+
+$(".woodsidepark").click(showModalthree)
+
+function showModalfour(){
+    $(".modal4").toggleClass("show")
+}
+
+$(".westfinchley").click(showModalfour)
+
+
+function showModalfive(){
+    $(".modal5").toggleClass("show")
+}
+
+$(".millhilleast").click(showModalfive)
+
+function showModalsix(){
+    $(".modal6").toggleClass("show")
+}
+
+$(".finchleycentral").click(showModalsix)
+
+function showModalseven(){
+    $(".modal7").toggleClass("show")
+}
+
+$(".eastfinchley").click(showModalseven)
+
+function showModaleight(){
+    $(".modal8").toggleClass("show")
+}
+
+$(".highgate").click(showModaleight)
+
+function showModalnine(){
+    $(".modal9").toggleClass("show")
+}
+
+$(".archway").click(showModalnine)
+
+function showModalten(){
+    $(".modal10").toggleClass("show")
+}
+
+$(".tufnellpark").click(showModalten)
+
+function showModaleleven(){
+    $(".modal11").toggleClass("show")
+}
+
+$(".kentishtown").click(showModaleleven)
+
+function showModaltwelve(){
+    $(".modal12").toggleClass("show")
+}
+
+$(".camdentown").click(showModaltwelve)
+
+function showModalthirteen(){
+    $(".modal13").toggleClass("show")
+}
+
+$(".mornington").click(showModalthirteen)
+
+function showModalfourteen(){
+    $(".modal14").toggleClass("show")
+}
+
+$(".chalkfarm").click(showModalfourteen)
+
+function showModalfifteen(){
+    $(".modal15").toggleClass("show")
+}
+
+$(".belsize").click(showModalfifteen)
+
+function showModalsixteen(){
+    $(".modal16").toggleClass("show")
+}
+
+$(".hampstead").click(showModalsixteen)
+
+function showModalseventeen(){
+    $(".modal17").toggleClass("show")
+}
+
+$(".golders").click(showModalseventeen)
+
+
+function showModaleighteen(){
+    $(".modal18").toggleClass("show")
+}
+
+$(".brentcross").click(showModaleighteen)
+
+function showModalnineteen(){
+    $(".modal19").toggleClass("show")
+}
+
+$(".hendon").click(showModalnineteen)
+
+function showModaltwenty(){
+    $(".modal20").toggleClass("show")
+}
+
+$(".colindale").click(showModaltwenty)
+
+
+function showModaltwentyone(){
+    $(".modal21").toggleClass("show")
+}
+
+$(".burntoak").click(showModaltwentyone)
+
+function showModaltwentytwo(){
+    $(".modal22").toggleClass("show")
+}
+
+$(".edgware").click(showModaltwentytwo)
+
+function showModaltwentythree(){
+    $(".modal23").toggleClass("show")
+}
+
+$(".euston").click(showModaltwentythree)
+
+function showModaltwentyfour(){
+    $(".modal24").toggleClass("show")
+}
+
+$(".kingscross").click(showModaltwentyfour)
+
+function showModaltwentyfive(){
+    $(".modal25").toggleClass("show")
+}
+
+$(".angel").click(showModaltwentyfive)
+
+
+function showModaltwentysix(){
+    $(".modal26").toggleClass("show")
+}
+
+$(".oldstreet").click(showModaltwentysix)
+
+function showModaltwentyseven(){
+    $(".modal27").toggleClass("show")
+}
+
+$(".moorgate").click(showModaltwentyseven)
+
+function showModaltwentyeight(){
+    $(".modal28").toggleClass("show")
+}
+
+$(".bank").click(showModaltwentyeight)
+
+function showModaltwentynine(){
+    $(".modal29").toggleClass("show")
+}
+
+$(".londonbridge").click(showModaltwentynine)
+
+function showModalthirty(){
+    $(".modal30").toggleClass("show")
+}
+
+$(".elephant").click(showModalthirty)
+
+function showModalthirtyone(){
+    $(".modal31").toggleClass("show")
+}
+
+$(".elephant").click(showModalthirtyone)
+
+
+function showModalthirtytwo(){
+    $(".modal32").toggleClass("show")
+}
+
+$(".kennington").click(showModalthirtytwo)
+
+function showModalthirty(){
+    $(".modal30").toggleClass("show")
+}
+
+$(".borough").click(showModalthirty)
+
+
+function showModalthirtythree(){
+    $(".modal33").toggleClass("show")
+}
+
+$(".oval").click(showModalthirtythree)
+
+function showModalthirtyfour(){
+    $(".modal34").toggleClass("show")
+}
+
+$(".stockwell").click(showModalthirtyfour)
+
+function showModalthirtyfive(){
+    $(".modal35").toggleClass("show")
+}
+
+$(".claphamnorth").click(showModalthirtyfive)
+
+function showModalthirtysix(){
+    $(".modal36").toggleClass("show")
+}
+
+$(".claphamcommon").click(showModalthirtysix)
+
+function showModalthirtyseven(){
+    $(".modal37").toggleClass("show")
+}
+
+$(".claphamsouth").click(showModalthirtyseven)
+
+function showModalthirtyeight(){
+    $(".modal38").toggleClass("show")
+}
+
+$(".balham").click(showModalthirtyeight)
+
+function showModalthirtynine(){
+    $(".modal39").toggleClass("show")
+}
+
+$(".tootingbec").click(showModalthirtynine)
+
+function showModalforty(){
+    $(".modal40").toggleClass("show")
+}
+
+$(".tootingbroadway").click(showModalforty)
+
+
+function showModalfortyone(){
+    $(".modal41").toggleClass("show")
+}
+
+$(".collierswood").click(showModalfortyone)
+
+
+function showModalfortytwo(){
+    $(".modal42").toggleClass("show")
+}
+
+$(".southwimbledon").click(showModalfortytwo)
+
+function showModalfortythree(){
+    $(".modal43").toggleClass("show")
+}
+
+$(".morden").click(showModalfortythree)
+
+function showModalfortytfour(){
+    $(".modal44").toggleClass("show")
+}
+
+$(".nineelms").click(showModalfortytfour)
+
+
+function showModalfortytfive(){
+    $(".modal45").toggleClass("show")
+}
+
+$(".battersea").click(showModalfortytfive)
+
+
+function showModalfortytsix(){
+    $(".modal46").toggleClass("show")
+}
+
+$(".waterloo").click(showModalfortytsix)
+
+function showModalfortytseven(){
+    $(".modal47").toggleClass("show")
+}
+
+$(".embankment").click(showModalfortytseven)
+
+function showModalfortyteight(){
+    $(".modal48").toggleClass("show")
+}
+
+$(".charingcross").click(showModalfortyteight)
+
+
+function showModalfortytnine(){
+    $(".modal49").toggleClass("show")
+}
+
+$(".leicester").click(showModalfortynine)
+
+function showModalfifty(){
+    $(".modal50").toggleClass("show")
+}
+
+$(".tottenham").click(showModalfifty)
+
+function showModalfiftyone(){
+    $(".modal51").toggleClass("show")
+}
+
+$(".goodge").click(showModalfiftyone)
+
+function showModalfiftytwo(){
+    $(".modal52").toggleClass("show")
+}
+
+$(".warren").click(showModalfiftytwo)
